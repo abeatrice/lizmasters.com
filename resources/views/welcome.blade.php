9ebe7841c5934ce7ea6e43d5f27d6eb4eee2909d
andrew beatrice <abeatrice.mail@gmail.com>
2019-07-02 02:18:06 +0000

    add flash message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,8 +17,11 @@
     <hr>
 
     @if (session('status'))
-        <div class="alert alert-success flash-message">
+        <div class="alert alert-success flash-message alert-dismissible fade show shadow-sm border border-success" role="alert">
             {{ session('status') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
         </div>
     @endif
 
