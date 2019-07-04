c88e08ce6aff0874c7e88e7984fa4794d5fc2da6
andrew beatrice <abeatrice.mail@gmail.com>
2019-07-04 04:53:43 +0000

    words on home for posts link

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 <div class="card" style="width: 18rem;">
     <div class="card-body">
       <h4 class="card-title text-center">Posts</h4>
-      <h6 class="card-subtitle mb-2 text-muted">Add, edit, or delete Images posted to the site. Only published posts will be displayed.</h6>
+      <h6 class="card-subtitle mb-2 text-muted">Add, edit, or delete Images posted to the site.</h6>
       <a href="/posts" class="btn btn-primary btn-lg btn-block">Posts</a>
     </div>
   </div>
