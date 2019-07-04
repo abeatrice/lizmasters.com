4721f4cdc28d97ad4eaf9679a76d2a0bd9cb48cb
andrew beatrice <abeatrice.mail@gmail.com>
2019-07-04 04:35:26 +0000

    add home to nav drop down

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="d-flex justify-content-center">
         <div class="flex-column text-center">
-            <img class="rounded-circle mt-0 mb-2" alt="Liz Masters" src="/storage/images/liz.png" height="80" width="80">
+            <img class="rounded-circle mt-0 mb-2" alt="Liz Masters" src="/storage/images/profile.png" height="80" width="80">
             <h2 class="mb-0">Liz Masters</h2>
             <h4 class="text-muted mb-2"><small>Burbank</small></h3>
             <h4 class="text-muted">Illustrator / Etsy Shop Owner / Emoji Designer </h4>
@@ -44,7 +44,8 @@
     <hr>
 
     <div class="d-flex-justify-content-center">
-        <p class="text-muted text-center"><small>Copyright © {{date('Y')}} Liz Masters</small></p>
+        <p class="text-muted text-center mb-0">Copyright © {{date('Y')}} Liz Masters</p>
+        <p class="text-muted text-center mt-0"><small>Site Created with &#9829; by <a href="http://andrewbeatrice.com">Andrew Beatrice</a></small></p>
     </div>
 
 </div>
